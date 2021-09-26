6eb10855961af786820de1cc254291933864ad65
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2021-09-26 18:57:43 +0700

    Update #3
    P2P

--- ROOM_SEVER/test.js
+++
@@ -1,7 +0,0 @@
-window.screenshot = require("screenshot-desktop");
-setInterval(function() {
-    screenshot().then((img) => {
-        var imgStr = new Buffer(img).toString('base64');
-        // io.to(id).emit('screen-data', imgStr);
-    })
-}, 100)
